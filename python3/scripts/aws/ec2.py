761d3ce0911b4eb50ceecf73f7bbf3a7a5b7553e
lrountree <lredvtree@gmail.com>
2023-08-04 15:58:33 -0700

    fixing logic issue in ec2.py

--- python3/scripts/aws/ec2.py
+++ python3/scripts/aws/ec2.py
@@ -13,7 +13,7 @@ def get_state(IN):
     try:
         Instance_State = ec2.state(IN)
     except:
-        print('Can not find state for instance ID: ', Instance_Id)
+        print('Can not find state for instance ID: ', IN)
         print(sys.exc_info()[1])
         sys.exit(1)
     if not Instance_State[0]:
@@ -26,33 +26,38 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Peform standard tasks against ec2 instances', prog='ec2')
     parser.add_argument('-p', action='store', type=str, default='default', help='AWS profile to use, from credentials file. Default is "default"')
     parser.add_argument('-r', action='store', type=str, default='us-west-2', help='AWS region, default is "us-west-2"')
-    parser.add_argument('-i', action='store', type=str, required=True, help='Instance identifier, either value of Name tag or instance ID')
+    parser.add_argument('-i', action='store', type=str, help='Instance identifier, the ec2 instance ID. Either -i or -n must be specified')
+    parser.add_argument('-n', action='store', type=str, help='Value of Name tag for the ec2 instance. Either -i or -n must be specified')
     parser.add_argument('-c', action='store', type=str, choices=['state', 'start', 'stop'], required=True, help='Command to issue to instance, options are: state, start, stop')
 
     args = parser.parse_args()
 
-    if args.p is False and args.r is False:
+    if args.p is False or args.r is False:
         parser.error('Profile (-p) and Region (-r) are REQUIRED')
+    elif args.i and args.n is False:
+        parser.error('Instance ID (-i) OR Instance Name (-n) MUST be specified!')
     else:
         ses = authmod.set_session(args.p, args.r)
         ec2 = ec2mod.ec2(ses)
         ec2_client = ses.client('ec2')
         ec2_info = ec2mod.ec2_get_info(ses)
 
-    check_value = ec2_info.instance_info_by_name(args.i)
-    if check_value[0] and check_value[1]:
-        instance_id = check_value[1][0]['Instances'][0]['InstanceId']
-    else:
-        instance_id = args.i
+    if args.n:
+        check_value = ec2_info.instance_info_by_name(args.n)
+
+        if check_value[0] and check_value[1]:
+            args.i = check_value[1][0]['Instances'][0]['InstanceId']
+        else:
+            print(check_value[1])
+            sys.exit(1)
 
     if args.c == 'state':
-        response = get_state(instance_id)
-        print(response[1])
+        print(get_state(args.i)[1])
     elif args.c == 'start':
-        response = ec2.start(instance_id)
+        response = ec2.start(args.i)
         print(response[1]['StartingInstances'][0]['CurrentState']['Name'])
     elif args.c == 'stop':
-        response = ec2.stop(instance_id)
+        response = ec2.stop(args.i)
         print(response[1]['StoppingInstances'][0]['CurrentState']['Name'])
 
 else:
